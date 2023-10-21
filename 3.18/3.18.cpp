9512c457462f26c14dd949910b9649b19abe9876
DSSETU <a0985254424@gmail.com>
2023-10-22 02:32:41 +0800

    [homework]

---
+++ 3.18/3.18.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	double weeklySales;
+	double weeklyIncome;
+
+	cout << "請輸入本周的總銷售額（美元）： ";
+	cin >> weeklySales;
+
+	weeklyIncome = 200.0 + (0.09 * weeklySales);
+
+	cout << "銷售人員本周的總收入為： $" << weeklyIncome << endl;
+
+	return 0;
+}
